91d041aeb017551c90e3e0e7dc21cfd711ee7841
Olson <bela.olson@nih.gov>
2026-02-05 10:06:38 -0500

    (#12) Configure ESLint and Prettier
    * created .npmrc
    * installed prettier and created .prettierrc
    * installed eslint with typescript parser and plugin
    * created eslint.config.mjs file
    * installed jsx-a11y plugin
    * installed eslint-plugin-simple-import-sort
    * set up husky pre-commit hooks
    * configured lint-staged
    * added npm scripts: lint, lint:fix, lint:src, format
    * created src folder and empty index.jsx file
    
    Closes #12

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+export default {
+  // Lint & Prettify TS and JS files
+  '*.{ts,tsx,js,jsx}': (filenames) => [
+    `eslint --fix ${filenames.join(' ')}`,
+    `prettier --write ${filenames.join(' ')}`,
+  ],
+  // Prettify only Markdown and JSON files
+  '*.{md,json,css,scss}': (filenames) =>
+    `prettier --write ${filenames.join(' ')}`,
+};
